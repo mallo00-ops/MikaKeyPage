5ea6e7f0b52ec93583ab7d0549cddac28a1789b7
Mallo <loidreyforge@gmail.com>
2025-07-01 06:15:11 +0800

    Generate_key.py

--- generate_key.py
+++ generate_key.py
@@ -1,16 +1,18 @@
 import hashlib, datetime, os
 
-# Get secret from GitHub Actions secret
+# Get secret from environment
 secret = os.getenv("MIKA_SECRET")
 if not secret:
-    raise Exception("MIKA_SECRET not found!")
+    raise Exception("MIKA_SECRET is not set!")
 
-days_since_epoch = int(datetime.datetime.utcnow().timestamp() // 86400)
-base = secret + str(days_since_epoch)
+# Generate day-based hash
+days = int(datetime.datetime.utcnow().timestamp() // 86400)
+base = secret + str(days)
 hash_val = hashlib.sha256(base.encode()).hexdigest()
 num = int(hash_val[:8], 16)
 key = f"Mika-{num % 10000:04d}"
 
+# Write key to file
 with open("todaykey.txt", "w") as f:
     f.write(key)
-  
+    
